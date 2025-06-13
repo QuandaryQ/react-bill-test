05b8322daf237277f4d4e8f449f8ccc5d70991ff
QuandaryQ <zhouzichen1997@foxmail.com>
2025-06-14 01:38:32 +0800

    修改样式

--- react-bill-test/src/pages/Layout/index.js
+++ react-bill-test/src/pages/Layout/index.js
@@ -1,14 +1,54 @@
-import {Outlet} from "react-router-dom";
-import {Button} from "antd-mobile";
+import {Outlet, useNavigate} from "react-router-dom";
+import {TabBar} from "antd-mobile";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {getBillList} from "@/store/modules/billStore";
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
+import './index.scss'
+
+const tabs = [
+    {
+        key: '/month',
+        title: '月度账单',
+        icon: <BillOutline />,
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: '/year',
+        title: '年度账单',
+        icon: <CalculatorOutline />,
+    },
+]
+
 
 const Layout=()=>{
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getBillList())
+    },[dispatch])
+    const navigate = useNavigate()
+    const switchRoute = (path) => {
+        navigate(path)
+    }
     return (
-        <div>
-            <Outlet/>
-            Layout框架
-            <Button color="primary">button 1</Button>
-            <div className="purple">
-                <Button color="primary">button 2</Button>
+        <div className="layout">
+            <div className="container">
+                <Outlet />
+            </div>
+            <div className="footer">
+                <TabBar onChange={switchRoute}>
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
             </div>
         </div>
     )
